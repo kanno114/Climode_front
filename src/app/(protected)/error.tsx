17d499bebf0a9f375636eedc16636a411f39c494
kanno <k.takumi114@gmail.com>
2026-02-16 16:44:47 +0900

    feat: 既存エラーバウンダリにSentryキャプチャを統合

--- src/app/(protected)/error.tsx
+++ src/app/(protected)/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { AlertTriangle } from "lucide-react";
@@ -13,6 +14,7 @@ export default function ProtectedError({
   reset: () => void;
 }) {
   useEffect(() => {
+    Sentry.captureException(error);
     console.error(error);
   }, [error]);
 
